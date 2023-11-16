2a7d92cc4d9880921dee41cc02114756419bbf27
ariiissleeping <34618870+ariissleeping@users.noreply.github.com>
2023-11-17 09:40:45 +1100

    Modified move keymapping, toggle hold down function for each individual move

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { startServer } from "./server";
-
 import { Move } from "cubing/alg";
 
-async function macOSPressKey(keyCode: number, seconds: number) {
+// Configuration for each key: true for press-down function, false for single press and release
+const keyConfig: Record<string, boolean> = {
+  L: false, // A
+  F: false, // S
+  R: false, // D
+  B: false, // W
+  D: false, // space
+  U: true, // O (left button)
+};
+
+// Current state of each key: true for key down, false for key up
+const keyState: Record<string, boolean> = {
+  L: false, // A
+  F: false, // S
+  R: false, // D
+  B: false, // W
+  D: false, // space
+  U: false, // O (left button)
+};
+
+async function macOSPressKey(keyCode: number, isKeyDown: boolean) {
   await Bun.spawn([
     "osascript",
     "-e",
@@ -13,25 +32,37 @@ async function macOSPressKey(keyCode: number, seconds: number) {
     return
   end if
 
-  key down ${keyCode}
-  delay ${seconds}
-  key up ${keyCode}
+  ${isKeyDown ? 'key down' : 'key up'} ${keyCode}
 end tell`,
   ]).exited;
 }
 
 // key codes from https://eastmanreference.com/complete-list-of-applescript-key-codes
 const moveMapping: Record<string, number> = {
-  L: 0,
-  F: 1,
-  R: 2,
-  B: 13,
+  L: 0, // A
+  F: 1, // S
+  R: 2, // D
+  B: 13, // W
+  D: 49, // space
+  U: 31, // O (left button)
 };
 
 async function onMove(move: Move) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 10));
   const keyCode = moveMapping[move.family];
-  macOSPressKey(keyCode, 0.1 * (move.amount + 2));
+
+  if (keyConfig[move.family]) {
+    // Toggle the state of the corresponding key and send a key down or key up event accordingly
+    keyState[move.family] = !keyState[move.family];
+    macOSPressKey(keyCode, keyState[move.family]);
+    console.log(`Toggled key '${move.family}' ${keyState[move.family] ? 'down' : 'up'}`);
+  } else {
+    // Single press and release
+    await macOSPressKey(keyCode, true);
+    console.log(`Pressed key '${move.family}' down`);
+    await macOSPressKey(keyCode, false);
+    console.log(`Released key '${move.family}'`);
+  }
 }
 
 startServer(onMove);
