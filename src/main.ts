4ad1bd86839e691eee63c8b3211dfd5c64e41bab
ariiissleeping <34618870+ariissleeping@users.noreply.github.com>
2023-11-17 18:46:50 +1100

    When undefined move is applied, attempts to do the opposite move and apply the keypress mapped to the opposite move. If not applied, it will skip.

--- src/main.ts
+++ src/main.ts
@@ -2,43 +2,27 @@ import { startServer } from "./server";
 import { Move } from "cubing/alg";
 import chalk from 'chalk';
 
-// Configuration for each key: true for press-down function, false for single press and release
-const keyConfig: Record<string, boolean> = {
-  "L": false, // A
-  "L'": false, // E
-  "F": false, // S
-  "R": false, // D
-  "B": false, // W
-  "D": false, // space
-  "U": true, // O (left button)
-  "U'": false, // P (right button)
-};
 
-// WCA color for each move
-const moveC: Record<string, chalk.Chalk> = {
-  "L": chalk.hex('#FF8800'),
-  "L'": chalk.hex('#FFD580'),
-  "F": chalk.green,
-  "R": chalk.red,
-  "B": chalk.blue,
-  "D": chalk.yellow,
-  "U": chalk.white,
-  "U'": chalk.magenta,
+// Move Configuration: pressdown (true = hold key toggle, false = single press), color: Colour shown in console, Applescript KeyCode
+const keyConfig = {
+  "L": { pressDown: false, color: chalk.hex('#FF8800'), keyCode: 0 }, // A
+  "L'": { pressDown: false, color: chalk.hex('#FFD580'), keyCode: 14 }, // E
+  "F": { pressDown: false, color: chalk.green, keyCode: 1 }, // S
+  "R": { pressDown: false, color: chalk.red, keyCode: 2 }, // D
+  "B": { pressDown: false, color: chalk.blue, keyCode: 13 }, // W
+  "D": { pressDown: false, color: chalk.yellow, keyCode: 49 }, // space
+  "U": { pressDown: true, color: chalk.white, keyCode: 31 }, // O (map to destroy block in Minecraft)
+  "U'": { pressDown: false, color: chalk.magenta, keyCode: 35 }, // P (map to use block in Minecraft)
 };
 
 let colorOutput = true; // on-off toggle for console color output
 
 // Current state of each key: true for key down, false for key up
-const keyState: Record<string, boolean> = {
-  "L": false, // A
-  "L'": false, // E
-  "F": false, // S
-  "R": false, // D
-  "B": false, // W
-  "D": false, // space
-  "U": false, // O (left button)
-  "U'": false, // P (right button)
-};
+const keyState: Record<string, boolean> = {};
+
+for (let key in keyConfig) {
+  keyState[key] = false;
+}
 
 async function macOSPressKey(keyCode: number, isKeyDown: boolean) {
   await Bun.spawn([
@@ -56,34 +40,41 @@ end tell`,
   ]).exited;
 }
 
-// key codes from https://eastmanreference.com/complete-list-of-applescript-key-codes
-const moveMapping: Record<string, number> = {
-  "L": 0, // A
-  "L'": 14, // E (inventory)
-  "F": 1, // S
-  "R": 2, // D
-  "B": 13, // W
-  "D": 49, // space
-  "U": 31, // O (left button, map to destroy block in Minecraft keybinds)
-  "U'": 35, // P (right button, map to use block in Minecraft keybinds)
-};
-
 async function onMove(move: Move) {
-  await new Promise((resolve) => setTimeout(resolve, 10));
+  await new Promise((resolve) => setTimeout(resolve, 5));
   let moveKey = move.family;
   if (move.amount === 2 || move.amount === -2) {
     moveKey += "2";
   } else if (move.amount === -1) {
     moveKey += "'";
   }
-  const keyCode = moveMapping[moveKey];
+
+  // Check if the move is defined
+  if (!keyConfig[moveKey]) {
+    console.log(`Move '${moveKey}' is not defined. Attempting to perform the opposite move.`);
+
+    // Try to perform the opposite move
+    if (moveKey.includes("'")) {
+      moveKey = moveKey.replace("'", "");
+    } else {
+      moveKey += "'";
+    }
+
+    // Check if the opposite move is defined
+    if (!keyConfig[moveKey]) {
+      console.log(`Opposite move '${moveKey}' is also not defined. Skipping this move.`);
+      return;
+    }
+  }
+
+  const keyCode = keyConfig[moveKey].keyCode;
   
-  if (keyConfig[move.family]) {
+  if (keyConfig[move.family].pressDown) {
     // Toggle the state of the corresponding key and send a key down or key up event accordingly
     keyState[move.family] = !keyState[move.family];
     macOSPressKey(keyCode, keyState[move.family]);
     if (colorOutput) {
-      console.log(moveC[move.family](`Toggled key ${keyCode} ${keyState[move.family] ? chalk.bold.underline('down') : chalk.bold.underline('up')}`));
+      console.log(keyConfig[move.family].color(`Toggled key ${keyCode} ${keyState[move.family] ? chalk.bold.underline('down') : chalk.bold.underline('up')}`));
     } else {
       console.log(`Toggled key '${keyCode}' ${keyState[move.family] ? 'down' : 'up'}`);
     }
@@ -91,19 +82,18 @@ async function onMove(move: Move) {
     // Single press and release
     await macOSPressKey(keyCode, true);
     if (colorOutput) {
-      console.log(moveC[move.family](`Pressed key ${keyCode} ` + chalk.bold.underline('down')));
+      console.log(keyConfig[move.family].color(`Pressed key ${keyCode} ` + chalk.bold.underline('down')));
     } else {
       console.log(`Pressed key '${keyCode}' down`);
     }
     await macOSPressKey(keyCode, false);
     if (colorOutput) {
-      console.log(moveC[move.family](chalk.bold.underline(`Released`) + ` key ${keyCode}`));
+      console.log(keyConfig[move.family].color(chalk.bold.underline(`Released`) + ` key ${keyCode}`));
     } else {
       console.log(`Released key '${keyCode}'`);
     }
   }
 }
-
 
 startServer(onMove);
 console.log("Visit: https://experiments.cubing.net/cubing.js/play/?go=keyboard&sendingSocketOrigin=ws%3A%2F%2Flocalhost%3A4001")
